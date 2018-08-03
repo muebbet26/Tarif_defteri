efa3f042520a0c26d49a15ab2e7c7ccf483d94b3
Tugba Akan <akan.tugba@gmail.com>
2018-08-03 16:53:44 +0300

    yorum satırları türkçe yapıldı

--- recipe_journal.py
+++ recipe_journal.py
@@ -1,31 +1,30 @@
-print('Developer: Tugba Akan')
+print('Kodyazar: Tugba Akan')
 print('\n')      
-print('Recipe Journal: A journal keeping your recipes in categories by the ingredients,')
-print('letting you enter a new recipe,')
-print('listing the recipes of the desired category')
+print('Tarif defteri: Tarif defterinizin dijital hali')
+print('Tariflerinizi tek bir yerde, kategorilerine göre saklayabilirsiniz.')
+print('Yeni tarifler ekleyebilir, dilediğiniz kategorideki tarifleri görüntüleyebilirsiniz.')
 
-#The recipes are taken from the website https://www.nefisyemektarifleri.com/
+#Tarifler şu adresten alındı: https://www.nefisyemektarifleri.com/
 
-#Version 1 : The categories are in Turkish: 'Et yemekleri, Tavuk yemekleri, Sebze yemekleri'
+#Versiyon 1 : Kategoriler: Et yemekleri, Tavuk yemekleri, Sebze yemekleri
 print('\n')
-print('if you would like to add a new recipe, put the text document into the folder that the code runs')
-print('and run the code. It will load all the text documents including the new one.')
-print('You can run the jupyter notebook')
-print('or the py file from the command line')
+print('Yeni bir tarif eklemek istediğinizde, tarifin olduğu text dosyasını kodun çalışacağı klasöre koyun')
+print('Kodu çalıştırdığınızda klasörde bulunan tüm text dosyalarını, yeni eklediğiniz dahil, okur.')
+print('Jupyter notebook olarak çalıştırabilir')
+print('ya da komut satırından py dosyasını çalıştırabilirsiniz.')
 
 #Python 3
-#the subjects used in the code are : 
-#importing text document and text mining
-#object oriented programming
+#Bu kodda kullanılan kavramlar şunlardır: 
+#text dosyası yükleme ve metin madeciliği
+#Nesne yönelimli programlama
 #Exception handling
 
-#create a class for categories
+#Kategori sınıfı yaratılıyor
 class Category(object):
     def __init__(self, ID, name, keywords):
         self.ID = ID
         self.name = name
         self.keywords = keywords
-
 
 cate1 = Category( ID = 1, name='Et yemekleri', keywords=[' et ', ' et\n', ' eti ', ' eti\n', 'kıyma',  'kıyması', 'kıyması'])
 cate2 = Category( ID = 2, name='Tavuk yemekleri', keywords= ['tavuk'])
@@ -33,7 +32,7 @@ cate3 = Category( ID = 3, name='Sebze yemekleri', keywords= ['patlıcan', 'semiz
 
 categories = [cate1, cate2, cate3 ]
 
-#create a class for recipes
+#Tarif sınıfı yaratılıyor
 class Recipe(object):
     def __init__(self,name, ingredients, categories):
         self.name = name
@@ -41,7 +40,7 @@ class Recipe(object):
         self.category = []
         self.setCategory(categories)
         
-    #method to set the category of the recipe
+    #tarifin kategorisi bulunuyor
     def setCategory(self, categories):
         for cate in categories:
             for ingr in self.ingredients:
@@ -49,8 +48,8 @@ class Recipe(object):
                     self.category.append(cate.name)
                     break
 
-#enter a new recipe as a txt document
-#we assume max 20 recipes will be given
+#eklemek istediginiz tarifi text dosyasına kaydedin
+#en fazla 20 tarif olacagini varsaydik
 recipes = []
 for i in range(1,20):
     try:
@@ -59,22 +58,22 @@ for i in range(1,20):
             rec = Recipe(name=new_recipe[0], ingredients = new_recipe[4::], categories = categories)
             recipes.append(rec)
     except IOError:
-        # This will only check for an IOError exception and then execute this print statement
-        print( "Error: Could not find file or read data" )
+        #Dosyanın olup olmadıgını kontrol eder
+        print( "Hata: dosya bulunamadi" )
     else:
-        print( "Content written successfully" )
+        print( "Dosya basariyla yuklendi" )
 
-#print the categories
+#Kategorileri goruntule
 #for item in categories:
 #    print(item.name)
 
-#print the name and the categories of the recipes
+#Tarif defterindeki tüm yemek isimlerini ve kategorilerini goruntule
 #for item in recipes:
 #    print(item.name)
 #    print(item.category)
 
-#list the recipes in the desired category
-print('Type the number of the category you wish to be listed')
+#Sectigin bir kategorideki tarifleri goruntule
+print('Goruntulemek istediginiz kategorinin numarasını yazın:')
 for item in categories:
     print( str(item.ID) + ' for ' + item.name)
 inp_cate_ID = input('')
@@ -85,5 +84,3 @@ for item in categories:
             for item3 in item2.category:
                 if item3 == item.name:
                     print(item2.name)
-
-                    
